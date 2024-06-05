9553395d0a32b8d117207e2be4af91427488bacb
Muhamad Ghandi Nur Setiawan <134348061+GhandiZero0X@users.noreply.github.com>
2024-06-06 02:56:09 +0700

    [+] Update Algoritma

--- app.py
+++ app.py
@@ -77,14 +77,12 @@ def diagnose():
     selected_symptoms = request.form.getlist('gejala')
     bagian_tanaman = request.form['bagian_tanaman']
     kondisi_iklim = request.form['kondisi_iklim']
-    intensitas_serangan = request.form['intensitas_serangan']
     hama_terlihat = request.form['hama_terlihat']
     tanda_penyakit = request.form['tanda_penyakit']
 
-    diagnosis_results = forward_chaining(selected_symptoms)  # Kirim gejala sebagai string ke fungsi forward_chaining
+    diagnosis_results = forward_chaining(selected_symptoms, bagian_tanaman)  # Kirim gejala sebagai string ke fungsi forward_chaining
     return render_template('result.html', nama=nama, kabupaten=kabupaten, hektar=hektar, gejala=selected_symptoms,
-                           bagian_tanaman=bagian_tanaman, kondisi_iklim=kondisi_iklim,
-                           intensitas_serangan=intensitas_serangan, hama_terlihat=hama_terlihat,
+                           bagian_tanaman=bagian_tanaman, kondisi_iklim=kondisi_iklim, hama_terlihat=hama_terlihat,
                            tanda_penyakit=tanda_penyakit, results=diagnosis_results)
 
 
